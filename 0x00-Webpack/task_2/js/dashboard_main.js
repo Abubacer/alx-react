0f76dccaf3d4ee93c037f5ca302d535ef488639d
Abubacer <azizdesignislife@gmail.com>
2024-01-30 00:16:18 +0000

    Adding CSS & Images

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,25 @@
+// import jquery and lodash
+import $ from 'jquery';
+import _ from 'lodash';
+
+// import css styling
+import '../css/main.css';
+
+// add elements to the body
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('Copyright - Holberton School');
+
+// tracks the number of times the button element has been clicked:
+// 1.bind the click event on the button to lodash debounce
+$('button').on('click', _.debounce(updateCounter, 500));
+// 2.init the count
+let count = 0;
+// 3.update the clicks counter
+function updateCounter() {
+        count++;
+        $('#count').text(`${count} clicks on the button`);
+}
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg|svg|webp)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, 
+              disable: true, 
+            },
+          },
+        ],
+      },
+    ]
+  },
+};
