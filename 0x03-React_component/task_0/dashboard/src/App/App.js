55d408eec3ad0b96b4f6d48a48bae1e5afa886ef
Abubacer <azizdesignislife@gmail.com>
2024-02-07 19:33:20 +0000

    Convert the App function into class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -10,24 +10,24 @@ import Notifications from "../Notifications/Notifications";
 import { getLatestNotification } from "../utils/utils";
 import CourseList from "../CourseList/CourseList";
 
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+];
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     const { isLoggedIn } = this.props;
-
-    const listCourses = [
-      { id: 1, name: "ES6", credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 },
-    ];
-
-    const listNotifications = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-    ];
 
     return (
       <React.Fragment>
