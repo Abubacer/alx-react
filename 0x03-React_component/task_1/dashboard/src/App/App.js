b2312ee5c7fc640d310affcf80365c27a8790745
Abubacer <azizdesignislife@gmail.com>
2024-02-07 22:45:25 +0000

    Add lifecycle to a component

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./App.css";
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
+import { getLatestNotification } from "../utils/utils";
+import CourseList from "../CourseList/CourseList";
+
+class App extends React.Component {
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+  ];
+
+  componentDidMount () {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (e) => {
+    if (e.ctrlKey && e.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={this.listNotifications} />
+        <Header />
+        {this.props.isLoggedIn ? (
+          <CourseList listCourses={this.listCourses} />
+        ) : (
+          <Login />
+        )}
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+export default App;
