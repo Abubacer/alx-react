f9dd7152f086780029696c1080376183fa59424a
Abubacer <azizdesignislife@gmail.com>
2024-02-21 00:53:16 +0000

    List and push

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 // accepts an array as parameter and converts it into an immutable List
-export function getListObject (array) {
+export function getListObject(array) {
   return List(array);
 }
 // appends the (element) string to the list and return the list
-export function addElementToList (list, element) {
+export function addElementToList(list, element) {
   return list.push(element);
 }
