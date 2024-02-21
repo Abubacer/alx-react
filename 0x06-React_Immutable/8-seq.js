c67a92f03bbf7102e7ea4f74c476ea70e490c2b9
Abubacer <azizdesignislife@gmail.com>
2024-02-21 18:31:20 +0000

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,7 +2,7 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
   const objectSeq = Seq(object);
-  const filteredObject = objectSeq.filter((student) => {student.score > 70});
+  const filteredObject = objectSeq.filter(student => student.score > 70);
 
   function capitalizedName(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
