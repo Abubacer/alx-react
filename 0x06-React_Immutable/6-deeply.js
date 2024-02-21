ba9e18a495799f6e7010cdbf66130d2f7621bfcf
Abubacer <azizdesignislife@gmail.com>
2024-02-21 16:59:35 +0000

    Nested merge

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,48 @@
+import { Map } from 'immutable';
+
+// merges two Immutable Maps deeply and returns a List containing the values of the two pages
+export function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
+}
+
+/* Example:
+const page1 = {
+  'user-1': {
+    id: 1,
+    name: 'test',
+    likes: {
+      1: {
+        uid: 1234,
+      }
+    }
+  },
+};
+
+const page2 = {
+  'user-1': {
+    likes: {
+      2: {
+        uid: 134,
+      }
+    }
+  },
+};
+
+mergeDeeplyElements(page1, page2).toJS();
+
+// It should return:
+const page1 = {
+  'user-1': {
+    id: 1,
+    name: 'test',
+    likes: {
+      1: {
+        uid: 1234,
+      },
+      2: {
+        uid: 134,
+      }
+    }
+  },
+};
+*/
