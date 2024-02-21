273ca522885b9b9fd31c67eb787fb27ab852ffa1
Abubacer <azizdesignislife@gmail.com>
2024-02-21 18:58:17 +0000

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,17 +2,19 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
   const objectSeq = Seq(object);
-  const filteredObject = objectSeq.filter(student => student.score > 70);
+  const filteredObject = objectSeq.filter((student) => student.score > 70);
 
-  function capitalizedName(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  const getStudent = filteredObject.toJS();
+
+  function capitalizedName(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
   }
 
-  const updatedStudents = filteredObject.map((student) => ({
-    ...student,
-    firstName: capitalizedName(student.firstName),
-    lastName: capitalizedName(student.lastName)
-  }));
+  Object.keys(getStudent).map((key) => {
+    getStudent[key].firstName = capitalizedName(getStudent[key].firstName);
+    getStudent[key].lastName = capitalizedName(getStudent[key].lastName);
+    return getStudent[key];
+  });
 
-  console.log(updatedStudents.toJS());
+  console.log(getStudent);
 }
