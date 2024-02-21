40cb7c4dbf3513d296f052a702e367301b960767
Abubacer <azizdesignislife@gmail.com>
2024-02-21 01:41:34 +0000

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -2,10 +2,16 @@ import { List, Map } from 'immutable';
 
 // returns a List containing the values of the two pages
 export function concatElements(page1, page2) {
-    return List(page1).concat(List(page2));
+    const listOne = List(page1);
+    const listTwo = List(page2);
+
+    return listOne.concat(listTwo);
 }
 
 // returns a List containing the values of the two pages
 export function mergeElements(page1, page2) {
-    return Map(page1).merge(Map(page2));
+    const mapOne = Map(page1);
+    const mapTwo = Map(page2);
+    
+    return mapOne.merge(mapTwo);
 }
