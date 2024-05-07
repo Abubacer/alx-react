42af4decbb7e38388ac896c04872505e03c35498
Abubacer <azizdesignislife@gmail.com>
2024-05-07 01:18:51 +0000

    redux reducer selector

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/uiReducer.test.js
@@ -0,0 +1,19 @@
+import uiReducer, { initialState } from './uiReducer';
+import { DISPLAY_NOTIFICATION_DRAWER } from '../actions/uiActionTypes';
+
+describe('uiReducer', () => {
+    it('returns the initial state when no action is passed', () => {
+        const state = uiReducer(undefined, {});
+        expect(state).toEqual(initialState);
+    });
+
+    it('returns the initial state when the action SELECT_COURSE is passed', () => {
+        const state = uiReducer(undefined, { type: 'SELECT_COURSE' });
+        expect(state).toEqual(initialState);
+    });
+
+    it('correctly changes the isNotificationDrawerVisible property when DISPLAY_NOTIFICATION_DRAWER action is passed', () => {
+        const state = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
+        expect(state.isNotificationDrawerVisible).toEqual(true);
+    });
+});
